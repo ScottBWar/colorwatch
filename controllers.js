37de92575db98594a6978b7d5e90c8aa25e460c1
Scott Warner <scottbwarner@gmail.com>
2015-09-25 12:23:47 -0400

    set up angular function

--- controllers.js
+++ controllers.js
@@ -2,24 +2,40 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', function MyController($scope, $http) {
 
-
-    $scope.stats = [{
+    
+	 $scope.stats = [{
         'label': 'str',
-        'value': 8
+        'value': 15
     }, {
         'label': 'dex',
-        'value': 8
+        'value': 12
     }, {
         'label': 'con',
-        'value': 8
+        'value': 10
     }, {
         'label': 'int',
-        'value': 8
+        'value': 10
     }, {
         'label': 'wis',
-        'value': 8
+        'value': 18
     }, {
         'label': 'cha',
-        'value': 8
+        'value': 14
     }];
+
+    $scope.makeCircle =	function(){var bodySelection = d3.select("body");
+ 
+ var svgSelection = bodySelection.append("svg")
+       .attr("width", 50)
+       .attr("height", 50);
+ 
+ var circleSelection = svgSelection.append("circle")
+      .attr("cx", 25)
+    .attr("cy", 25)
+     .attr("r", 25)
+    .style("fill", "purple");
+  };
+
+
+
 });
