9613ed09088a26f2073547f468e318b0b45e8f27
Scott Warner <scottbwarner@gmail.com>
2015-09-27 16:58:41 -0400

    directive plots one stat at a time

--- controllers.js
+++ controllers.js
@@ -73,64 +73,41 @@ myApp.directive('wark', function() {
 myApp.directive('simpleChart', function($window) {
     return {
         restrict: 'EA',
+        // scope: {
+        //     data: '=' // bi-directional data-binding
+        // },
         // template: "<svg width='850' height='200'></svg>",
         link: function(scope, elem, attrs) {
-            var dataToPlot = scope[attrs.chartData];
+
+
+            var statToDraw = scope[attrs.chartData];
+
+            // scope.$watch('dataToPlot', function(newVals, oldVals) {
+            //     return drawSimpleChart(newVals);
+            // }, true);
 
             function drawSimpleChart() {
                 var bodySelection = d3.select(elem[0]);
 
-            
-                for (var i = 0; i < dataToPlot.length; i++) {
 
                     var svgSelection = bodySelection.append("svg")
-                    .attr("width", 100)
-                    .attr("height", 100);
+                        .attr("width", 100)
+                        .attr("height", 100);
 
 
 
                     var circleSelection = svgSelection.append("circle")
                         .attr("cx", 50)
                         .attr("cy", 50)
-                        .attr("r", dataToPlot[i].value * 2)
-                        .style("fill", dataToPlot[i].color);
-
-                }
-
-                // var svgContainer = d3.select("body").append("svg")
-                //     .attr("width", 600)
-                //     .attr("height", 100);
-
-                // var circles = svgContainer.selectAll("circle")
-                //     .data(circleRadii)
-                //     .enter()
-                //     .append("circle")
-
-                // var circleAttributes = circles
-                //     .attr("cx", 50)
-                //     .attr("cy", 50)
-                //     .attr("r", function(d) {
-                //         return d;
-                //     }).style("fill", function(d) {
-                //         var returnColor;
-                //         if (d === 40) {
-                //             returnColor = "green";
-                //         } else if (d === 20) {
-                //             returnColor = "purple";
-                //         } else if (d === 10) {
-                //             returnColor = "red";
-                //         }
-                //         return returnColor;
-                //     });
-
-
-
-
+                        .attr("r", statToDraw.value * 2)
+                        .style("fill", statToDraw.color);
 
             }
 
             drawSimpleChart();
         }
+
+
     };
 });
 
